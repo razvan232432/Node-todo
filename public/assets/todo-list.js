0940ae08c3aea54449661604cf20c426bc988ac0
razvan232432 <marius3837@proton.me>
2023-06-27 09:47:27 -0700

    Update todo-list.js

---
+++ public/assets/todo-list.js
@@ -0,0 +1,34 @@
+$(document).ready(() => {
+
+    $('form').on('submit', () => {
+  
+        let item = $('form input');
+        let todo = {item: item.val()};
+  
+        $.ajax({
+          type: 'POST',
+          url: '/todo',
+          data: todo,
+          success: (data) => {
+            //do something with the data via front-end framework
+            location.reload();
+          }
+        });
+  
+        return false;
+  
+    });
+  
+    $('li').on('click', () => {
+        let item = $(this).text().replace(/ /g, "-");
+        $.ajax({
+          type: 'DELETE',
+          url: '/todo/' + item,
+          success: (data) => {
+            //do something with the data via front-end framework
+            location.reload();
+          }
+        });
+    });
+  
+  });

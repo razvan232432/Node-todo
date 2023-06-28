3bb56d8b0eff5d9db79b9e8ef9be0e026beb8ab9
razvan232432 <marius3837@proton.me>
2023-06-28 07:54:32 -0700

    Update todoControllers.js

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,52 +1,44 @@
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const mongoose = require('mongoose')
-
-// connect to the  database
+// connect to the database
 mongoose.connect('mongodb://localhost:27017/todo');
 
-//create a  schema - this is like  a blue print
-
-let todoSchema = new mongoose.Schema({
-    item: String
+// create a schema - this is like a blueprint
+const todoSchema = new mongoose.Schema({
+  item: String
 });
 
 const Todo = mongoose.model('Todo', todoSchema);
-const createAndSaveItem = async () => {
-    const itemOne = new Todo({ item: 'buy flowers' });
-    
+
+//const data = [{item: 'get milk'}]
+
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+module.exports =  (app) => {
+  app.get('/todo', async (req, res) => {
     try {
-      await itemOne.save();
+      const data = await Todo.find({});
+      res.render('todo', { todo: data });
     } catch (err) {
       throw err;
     }
+  });
   
+  app.post('/todo', urlencodedParser, async (req, res) => {
+    try {
+      const newTodo = await Todo(req.body).save();
+      res.json(newTodo);
+    } catch (err) {
+      throw err;
+    }
+  });
   
-  try {
-    await createAndSaveItem();
-  } catch (err) {
-    done(err);
-  }
-};
-let  urlencodedParser = bodyParser.urlencoded({extended: false});
-
-module.exports = (app) => {
-    app.get('/todo', (req, res) => {
-        res.render('todo', {todo: data});
-    });
-
-    app.post('/todo', urlencodedParser, (req, res) => {
-        data.push(req.body);
-        res.json(data);
-    });    
-    
-    app.delete('/todo/:item', (req, res) => {
-        data = data.filter((todo) =>{
-            return todo.item.replace(/ /g, '-') !== req,params.items
-        })
-        res.json(data)
-    });
-
-
-
-};
+  app.delete('/todo/:item', async (req, res) => {
+    try {
+      const data = await Todo.find({ item: req.params.item.replace(/\-/g, " ") }).findOneAndRemove();
+      res.json(data);
+    } catch (err) {
+      throw err;
+    }
+  })};

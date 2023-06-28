b20da7e1688830f3f2e346acb6a4e519ae5ec314
razvan232432 <marius3837@proton.me>
2023-06-28 06:25:28 -0700

    Update todoControllers.js

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -2,15 +2,6 @@ const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose')
 
-const mongoURI = "Connection URI";
-
-const connectToMongo = async () => {
- mongoose.connect(mongoURI, await console.log('Connected to mongo Successful')
-    );
-}
-
-module.exports = connectToMongo
-
 // connect to the  database
 mongoose.connect('mongodb://localhost:27017/todo');
 
@@ -20,21 +11,28 @@ let todoSchema = new mongoose.Schema({
     item: String
 });
 
-let Todo = mongoose.model('Todo', todoSchema);
-
-let itemOne = Todo({item: 'buy flowers'}).save((err) => {
-    if (err) throw err;
-    console.log('item saved')
-});
-
-
-let data = [{item:'get milk'}, {item: 'walk dog'}, {item: 'kick some coding'}];
-
+const Todo = mongoose.model('Todo', todoSchema);
+const createAndSaveItem = async () => {
+    const itemOne = new Todo({ item: 'buy flowers' });
+    
+    try {
+      await itemOne.save();
+    } catch (err) {
+      throw err;
+    }
+  
+  
+  try {
+    await createAndSaveItem();
+  } catch (err) {
+    done(err);
+  }
+};
 let  urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = (app) => {
     app.get('/todo', (req, res) => {
-        res.render('todo', {todos: data});
+        res.render('todo', {todo: data});
     });
 
     app.post('/todo', urlencodedParser, (req, res) => {
